036d349d56ce0b3faf996726280b7d8c09145479
Samuel <obajisamuelekene@gmail.com>
2019-11-03 04:31:46 +0100

    Completed the second module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
-import { rxjs/add/operator/map } from '@angular';
+import 'rxjs/add/operator/map';
 
 
 @Injectable()
@@ -13,7 +13,7 @@ export class ProductService {
 
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl);
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 
 }
